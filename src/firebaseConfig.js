3620aa578c01871bfd8c08f4c1028aed4e0aa7ce
techycereal <alexjmilliken@outlook.com>
2024-09-03 14:35:01 -0400

    firebase change

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA5zfaBeqhWxTLiTgcGZOtjdf8ZP7M7gP0",
-  authDomain: "chatapp-e26b8.firebaseapp.com",
-  projectId: "chatapp-e26b8",
-  storageBucket: "chatapp-e26b8.appspot.com",
-  messagingSenderId: "483622305554",
-  appId: "1:483622305554:web:5d5247d39c6a978c6a3dcb",
-  measurementId: "G-7QG0LNT8WZ"
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.MESSAGE_SENDER}`,
+  appId: `${process.env.APP_ID}`,
+  measurementId: `${process.env.MEASUREMENT_ID}`
 };
 
 // Initialize Firebase
